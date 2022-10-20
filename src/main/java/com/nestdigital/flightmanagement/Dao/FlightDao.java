9b7b527be0586a08dee42c1b842c69b2b95ee963
kishore951 <kishoresakthivel951@gmail.com>
2022-10-20 14:34:10 +0530

    no

--- src/main/java/com/nestdigital/flightmanagement/Dao/FlightDao.java
+++ src/main/java/com/nestdigital/flightmanagement/Dao/FlightDao.java
@@ -1,7 +1,19 @@
 package com.nestdigital.flightmanagement.Dao;
 
 import com.nestdigital.flightmanagement.Model.FlightModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface FlightDao extends CrudRepository<FlightModel,Integer>{
+
+    @Modifying
+    @Query(value = "DELETE FROM `flights` WHERE `id`=:id",nativeQuery = true)
+    void deleteFlightById(Integer id);
+
+
+    @Query(value ="SELECT `id`, `capacity`, `destination`, `name`, `origin` FROM `flights` WHERE `name`=:name",nativeQuery = true)
+    List<FlightModel> searchFlight(String name);
 }
